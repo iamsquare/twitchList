780e691b9408c35b18a3a47aeff4520957b134cb
squaremarco <squaremarco@gmail.com>
2016-07-02 13:52:19 +0200

    minified js

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ var app = angular.module("twitchList");
 
 var MainController = function(TwitchListFactory){
 
-	var channels = ["freecodecamp", "sing_sing", "sheevergaming", "comster404", "zai"]; //.concat(TwitchListFactory.splitAddressSearch());
+	var channels = ["freecodecamp", "sing_sing", "sheevergaming", "comster404", "zai"].sort(); //.concat(TwitchListFactory.splitAddressSearch());
 
 	var results = this.results = [];
 
